b0a398b9453b90e3f89850a94a5c4a028a02d200
aSquidsBody <aSquidsBody@gmail.com>
2021-04-09 23:36:53 -0400

    udemy dump

---
+++ typescript/features/annotations/variables.ts
@@ -0,0 +1,22 @@
+let apples: number = 5;
+let speed: string = "ok";
+
+let nothing: undefined = undefined;
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+
+// Classes
+class Car {}
+
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
